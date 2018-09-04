f0fe1e88f3f497b762fbac557f83ee320280aed1
dream <com_caoyun@163.com>
2018-09-04 23:52:45 +0800

    子线程创建handler

---
+++ app/src/main/java/com/my_project/test_more_thread/UIThread2ChildThreadCommunicationActivity.java
@@ -0,0 +1,40 @@
+package com.my_project.test_more_thread;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by com_c on 2018/9/4.
+ */
+
+public class UIThread2ChildThreadCommunicationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        MyThread childThread = new MyThread();
+        childThread.start();
+        HandlerThread handlerThread = new HandlerThread("handlerThread");
+        Handler mHandler = new Handler(handlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+            }
+        };
+    }
+
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+}
