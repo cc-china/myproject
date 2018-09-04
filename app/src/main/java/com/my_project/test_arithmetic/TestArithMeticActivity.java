fbcb048e1dbcb6324624d0a80105c5e0296bd8d7
dream <com_caoyun@163.com>
2018-09-04 17:08:45 +0800

    冒泡排序，选择排序，归并排序，快速排序，二分法查找，反转链表

--- app/src/main/java/com/my_project/test_arithmetic/TestArithMeticActivity.java
+++ app/src/main/java/com/my_project/test_arithmetic/TestArithMeticActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.my_project.R;
 
 /**
  * Created by com_c on 2018/8/24.
@@ -13,52 +16,27 @@ public class TestArithMeticActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        initView1();
+//        setContentView(R.layout.test_activity);
         initView2();
     }
 
     private void initView2() {
-        //快速排序
-//        int[] arr = {12, 5, 8, 21, 87, 3, 4, 1, 76, 30, 25, 9};
-        int[] arr1 = {0,1, 2, 5, 8};
-        int[] arr2 = { 1, 2, 5, 8};
-        SortUtils.quickSort(arr1, arr2);
-    }
 
-    private void initView1() {
-        /**
-         * 冒泡排序就是两两比较数组元素，大的元素和小的元素互换位置，最终达到有序数组的目的
-         * 冒泡排序的规则：
-         * 1、外层for循环控制循环多少次，那一个数组需要循环多少次，应当是它的length - 1，
-         *    因为 下标0和1去对比（假设只有三个元素）1和2去对比，就完了，
-         * 2、内层循环控制一趟对比，比较多少次，为什么不直接从头对比到结尾？
-         *    因为每一躺两两比较数组元素，一定会找到数组最大的元素，而这个最大的数组元素是确定已知的，
-         *    无需再次放到数组中去对比，
-         *    例如：第一次对比后，数组最后一位就是排序正确最大的数组元素，固定
-         *         第二次对比后，数组倒数第二位就是排序正确倒数第二的数组元素，固定
-         *         第三次对比后，数组倒数第三位就是排序正确倒数第三的数组元素，固定
-         *                            。。。
-         *     所以，内层循环不需要每次都从头到尾的去对比，每次对比后找出的固定元素，下次对比排序就不在参与
-         *     所以内层循环的循环次数，就需要减去当前是第几次循环，因为第几次循环，它就会确定固定了第几个最大值
-         * 3、在对比两个元素的时候，if判断arr[i]和arr[i+1] 如果满足arr[i]>arr[i+1]，用一个局部变量记录
-         *    最大值，也就是temp = arr[i],然后将arr[i+1](也就是两个数中较小的那个)赋值给arr[i]
-         *    最后将temp赋值给arr[i+1]，来完成数组元素位置的互换
-         * */
         //冒泡排序
         int[] arr = {12, 5, 8, 21, 87, 3, 4, 1, 76, 30, 25, 9};
-        int temp = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                Log.e("111_arr------", arr[i] + "");
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            Log.e("111_arr", arr[i] + "");
-        }
+        SortUtils.bubbleSort(arr);
+
+        //选择排序
+        SortUtils.chooseSort(arr);
+
+        //快速排序
+        int[] arr1 = {0, 1, 2, 5, 8};
+        int[] arr2 = {1, 2, 5, 8};
+        SortUtils.mergeSort(arr1, arr2);
+        int result = SortUtils.binarySearch(arr1, 3);
+        Toast.makeText(this, result + "", Toast.LENGTH_SHORT).show();
+
+
     }
+
 }
